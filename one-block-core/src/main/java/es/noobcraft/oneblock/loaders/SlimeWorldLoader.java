1c4e37913ba6dbae239b107c9c22f2bb29cb620b
Brouse13 <unaiunai2004@gmail.com>
2022-05-22 19:32:20 +0200

    Fixed(World): Now when all the coops unload the island, the visitors get kicked

--- one-block-core/src/main/java/es/noobcraft/oneblock/loaders/SlimeWorldLoader.java
+++ one-block-core/src/main/java/es/noobcraft/oneblock/loaders/SlimeWorldLoader.java
@@ -1,17 +1,23 @@
 package es.noobcraft.oneblock.loaders;
 
+import com.google.common.collect.Sets;
 import com.grinderwolf.swm.api.SlimePlugin;
 import com.grinderwolf.swm.api.exceptions.*;
 import com.grinderwolf.swm.api.world.SlimeWorld;
 import com.grinderwolf.swm.api.world.properties.SlimeProperties;
 import com.grinderwolf.swm.api.world.properties.SlimePropertyMap;
+import es.noobcraft.oneblock.api.OneBlockAPI;
 import es.noobcraft.oneblock.api.loaders.WorldLoader;
 import es.noobcraft.oneblock.api.logger.Logger;
 import es.noobcraft.oneblock.api.logger.LoggerType;
+import es.noobcraft.oneblock.api.player.OneBlockPlayer;
 import org.bukkit.Bukkit;
 import org.bukkit.World;
+import org.bukkit.entity.Player;
 
 import java.io.IOException;
+import java.util.List;
+import java.util.Set;
 
 public class SlimeWorldLoader implements WorldLoader {
     private final SlimePlugin slimePlugin = ((SlimePlugin) Bukkit.getPluginManager().getPlugin("SlimeWorldManager"));
@@ -50,6 +56,19 @@ public class SlimeWorldLoader implements WorldLoader {
         if (world == null) {
             Logger.log(LoggerType.ERROR, "World is already unloaded");
             return false;
+        }
+
+        //Teleport all te players to the lobby if the
+        List<Player> onlinePlayers = Bukkit.getWorld(worldName).getPlayers();
+        Set<OneBlockPlayer> players = Sets.newHashSet();
+
+        onlinePlayers.stream().map(player -> OneBlockAPI.getPlayerCache().getPlayer(player.getName())).forEach(players::add);
+
+        if (players.size() == onlinePlayers.size()) {
+            players.forEach(player -> {
+                player.getBukkitPlayer().teleport(OneBlockAPI.getSettings().getLobbySpawn());
+                Logger.player(player.getNoobPlayer(), "one-block.messages.world-unloading");
+            });
         }
 
         if (!Bukkit.unloadWorld(world, true)) {
